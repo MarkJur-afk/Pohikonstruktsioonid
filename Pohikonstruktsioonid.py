1d1a49993e21bee76419f3368f00b05a3df188f1
opilane <opilane@HP-CZC2349HV0>
2025-01-20 11:26:40 +0200

    print(), type((), int(), input(), (20.01.25)

---
+++ Pohikonstruktsioonid.py
@@ -0,0 +1,15 @@
+print("hello world","hello2" , end="...", sep="---") #сепаратор разделяет пробел, end вписывате конец 
+#andmetüübid täisarv -int
+#ujukomaarv -float
+#sõne -str
+#tõevärtused -bool
+#astendamine 
+a=5 
+aste=2
+tulemus=a**aste
+print(a, "**", aste, "=", tulemus)
+#korrutamine *
+arv1=int(input("Esimine number:"))
+arv2=int(input("Teine number:"))
+answer=arv1*arv2
+print(arv1, "*", arv2, "=", )
---
+++ Pohikonstruktsioonid2/Pohikonstruktsioonid2.py
@@ -0,0 +1,30 @@
+a=5 
+aste=2
+tulemus=a**aste
+print(a, "**", aste, "=", tulemus)
+#korrutamine *
+# arv1=int(input("Esimine number:"))
+# arv2=int(input("Teine number:"))
+# answer=arv1*arv2
+# print(arv1, "*", arv2, "=", answer)
+#jagamine
+# arv3=int(input("Esimine number:"))
+# arv4=int(input("Teine number:"))
+# answer=arv3/arv4
+# print(arv3, "/", arv4, "=", answer)
+# print(type(answer))
+arv1=int(input("esimene number: "))
+arv2=int(input("teine number: "))
+answer=arv1+arv2
+print(arv1, "+", arv2, "=", answer)
+print(type(answer))
+arv3=int(input("esimene number: "))
+arv4=int(input("teine number: "))
+answer=arv3-arv4
+print(arv3, "-", arv4, "=", answer)
+print(type(answer))
+arv5=int(input("esimene number: "))
+arv6=int(input("teine number: "))
+answer=arv5//arv6
+print(arv5, "//", arv6, "=", answer)
+print(type(answer))
