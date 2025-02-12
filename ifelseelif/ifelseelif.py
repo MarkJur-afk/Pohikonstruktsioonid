9568d2757c148cfcdc2517f9199161a9c284af41
MarkJur-afk <jurgenmarkk@gmail.com>
2025-02-12 15:05:01 +0200

    ifelseelif

---
+++ ifelseelif/ifelseelif.py
@@ -0,0 +1,22 @@
+#Maidis 1
+from random import *
+n = randint(1, 10)
+print (n)
+if n > 5:
+    print("*************")
+    print(f"Arv {n} suurem kui viis ")
+    print("*************")
+
+arv = randint(-10, 10)
+
+if arv > 0:
+    print(f"+")
+else:
+    print("-")
+
+if arv > 0:
+    print("+")
+elif arv == 0:
+    print ("0")
+else:
+    print("-")
